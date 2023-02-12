fcbb706ad74c61a3138b9d6947ed1d3830ffaaf8
nicholas <tohnic1999@gmail.com>
2023-02-13 00:29:07 +0800

    Homepage skeleton + header

---
+++ resources/views/home_page.blade.php
@@ -0,0 +1,13 @@
+@extends('master')
+
+@section('title', 'Supplycart - Home')
+
+@section('body')
+    <div class="fixed bg-amber-400 h-32 w-full">
+        <div class="w-24 h-20 flex justify-center items-center">
+            <div class="w-16">
+                <img src="{{ asset('/images/supplycart-logo.png') }}">
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>@yield('title')</title>
+</head>
+
+<body>
+    @yield('body')
+</body>
+
+<script type="text/javascript" src=""></script>
+
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+
+@stack('css')
+@stack('script')
+
+</html>
