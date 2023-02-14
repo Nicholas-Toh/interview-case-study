12bc21c79c4d2da375602574415cbfefe8279843
nicholas <tohnic1999@gmail.com>
2023-02-14 23:52:18 +0800

    Improve login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,31 +1,46 @@
 @extends('master')
 
-@section('title', 'Supplycart - Registration')
+@section('title', 'Supplycart - Login')
+
+@section('body-class', 'bg-gray-200 h-full w-full m-0')
 
 @section('body')
+    <div class="flex justify-center content-center h-full w-full">
+        <div class="m-auto w-full max-w-xs bg-white px-8 py-10 shadow-md rounded">
+            <div class="mb-4">
+                <p class="text-2xl">
+                    Login
+                </p>
+            </div>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="mb-4">
+                    <label class="block" for="email">Email</label>
+                    <input id="email" name="email" type="email"
+                        class="border border-slate-400 rounded @error('email') focus:border-rose-600 @else focus:border-sky-400 @enderror"
+                        placeholder="john@example.com" required>
+                    @error('email')
+                        <p class="text-rose-600 dark:text-rose-500 text-sm">{{ $message }}</p>
+                    @enderror
+                </div>
 
-    <div class="fixed bg-amber-400 h-32 w-full">
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <label for="email">Email</label>
-            <input id="email" name="email" type="email"
-                class="border border-slate-400 @error('email') focus:border-rose-600 @else focus:border-sky-400 @enderror"
-                placeholder="john@example.com" required>
-            @error('email')
-                <span class="text-rose-600 dark:text-rose-500 text-sm">{{ $message }}</span>
-            @enderror
-
-            <label for="password">Password</label>
-            <input id="password" name="password" type="password"
-                class="border border-slate-400 @error('password') focus:border-rose-600 @else focus:border-sky-400 @enderror"
-                required>
-            @error('password')
-                <span class="text-rose-600 dark:text-rose-500 text-sm">{{ $message }}</span>
-            @enderror
-        </form>
-        @error('email')
-            <span class="text-rose-600 dark:text-rose-500 text-sm">{{ $message }}</span>
-        @enderror
+                <div class="mb-4">
+                    <label class="block" for="password">Password</label>
+                    <input id="password" name="password" type="password"
+                        class="border border-slate-400 rounded @error('password') focus:border-rose-600 @else focus:border-sky-400 @enderror"
+                        required>
+                    @error('password')
+                        <p class="text-rose-600 dark:text-rose-500 text-sm">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="flex items-center justify-between">
+                    <button type="submit"
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="button">
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 @endsection
