aafce0d82871da70e3651392d63301dd7a53f905
nicholas <tohnic1999@gmail.com>
2023-02-18 14:47:24 +0800

    Chaneg foreign column name

--- database/migrations/2023_02_15_114053_create_product_variations_table.php
+++ database/migrations/2023_02_15_114053_create_product_variations_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('product_variations', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('parent_id')->constrained('products')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
             $table->string('sku')->unique();
             $table->decimal('price', 14, 4)->default(0);
             $table->timestamps();
